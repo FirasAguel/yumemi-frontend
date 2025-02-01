194f23e7c0080dc0e0b71b67fb0a49a7ca732941
Mohamed Firas Aguel <firas.aguel@gmail.com>
2025-02-01 15:37:27 +0900

    chore: run prettier

--- src/components/PrefectureSelector.tsx
+++ src/components/PrefectureSelector.tsx
@@ -8,16 +8,21 @@ export default function PrefectureSelector({
   return (
     <>
       <div className="flex flex-wrap w-108 space-x-2">
-        {prefectures.map((prefecture: { prefCode: number; prefName: string }) => {
-          return (
-            <div key={prefecture.prefCode} className="flex space-x-2 w-25">
-              <input type="checkbox" id={`pref-${prefecture.prefCode}`}></input>
-              <label htmlFor={`pref-${prefecture.prefCode}`}>
-                {prefecture.prefName}
-              </label>
-            </div>
-          );
-        })}
+        {prefectures.map(
+          (prefecture: { prefCode: number; prefName: string }) => {
+            return (
+              <div key={prefecture.prefCode} className="flex space-x-2 w-25">
+                <input
+                  type="checkbox"
+                  id={`pref-${prefecture.prefCode}`}
+                ></input>
+                <label htmlFor={`pref-${prefecture.prefCode}`}>
+                  {prefecture.prefName}
+                </label>
+              </div>
+            );
+          }
+        )}
       </div>
     </>
   );
