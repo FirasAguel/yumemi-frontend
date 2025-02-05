4a2ab747859ca451c521c0ed223aac2d529f635c
Mohamed Firas Aguel <firas.aguel@gmail.com>
2025-02-05 22:21:12 +0900

    feat: lift selectedPrefectures state to ChartController

--- src/components/ChartController.tsx
+++ src/components/ChartController.tsx
@@ -1,18 +1,25 @@
+'use client';
 import PrefectureSelector from '@/components/PrefectureSelector';
 import PopulationChart from '@/components/PopulationChart';
 import { Prefecture } from '@/types/interfaces';
+import { useState } from 'react';
 
 export default function ChartController({
   prefectures,
 }: {
   prefectures: Prefecture[];
 }) {
+  const [selectedPrefectures, setSelectedPrefectures] = useState<number[]>([]);
   return (
     <div className="flex flex-1 flex-col items-center justify-center w-full">
-      <PrefectureSelector prefectures={prefectures} />
+      <PrefectureSelector
+        prefectures={prefectures}
+        selectedPrefectures={selectedPrefectures}
+        onSelectionChange={setSelectedPrefectures}
+      />
       <div className="w-1/2 max-w-4xl h-auto p-4">
         <h2>人口構成グラフ</h2>
-        <PopulationChart />
+        <PopulationChart selectedPrefectures={selectedPrefectures} />
       </div>
     </div>
   );
