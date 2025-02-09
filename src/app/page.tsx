0acb071ed47550a5afe7442854ffa518542d54ff
Mohamed Firas Aguel <firas.aguel@gmail.com>
2025-02-09 12:42:10 +0900

    chore: remove placeholder text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,10 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="p-5 text-center text-xl font-semibold">
-        <h1>Welcome to my Yumemi frontend test submission</h1>
+      <header className="p-5 text-center text-4xl font-light">
+        <h1>都道府県人口グラフ</h1>
       </header>
       <main className="flex flex-1 flex-col items-center justify-center">
-        <p className="text-gray-700 dark:text-gray-300">
-          This is a minimal Next.js setup.
-        </p>
         <ChartController prefectures={prefectures} />
       </main>
       <footer className="p-5 text-center text-sm text-gray-500">
